a8e2c2461282adae1e9e265455ca086dd4ae67c6
Maksim Balashov <tracerb@inbox.ru>
2024-01-10 21:58:05 +0300

    Добавил документацию к BackendApplication

--- src/main/java/com/tacrolimus/backend/BackendApplication.java
+++ src/main/java/com/tacrolimus/backend/BackendApplication.java
@@ -3,9 +3,15 @@ package com.tacrolimus.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Maksim Balashov
+ * Основной класс
+ */
 @SpringBootApplication
 public class BackendApplication {
-
+	/**
+	 * @param args аргумент
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
